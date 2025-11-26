033f9e3c567d42a34e08ce18d059782182239de8
Raphael Owino <raphael@ralphowino.com>
2025-11-26 06:41:41 +0300

    chore: move migrations to src/db/migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  out: "./migrations",
+  out: "./src/db/migrations",
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
